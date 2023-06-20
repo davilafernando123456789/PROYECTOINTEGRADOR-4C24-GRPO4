36f1c204269479f0137788dc196a97cb47007733
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2023-06-19 23:36:19 -0500

    subiendo fronend de login react-django

--- PROYECTOINTEGRADOR-4C24-GRPO4/FRONTEND/ADMIN/FRONTEND CRUD-REACT/my-app-PI/src/components/Login.js
+++ PROYECTOINTEGRADOR-4C24-GRPO4/FRONTEND/ADMIN/FRONTEND CRUD-REACT/my-app-PI/src/components/Login.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Button, FormGroup } from 'reactstrap';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import '../App.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -19,7 +20,7 @@ const Login = () => {
         // Redirigir a la vista Menu
         history.push('/menu');
       } else {
-        alert('Error: ' + response.status)
+        alert('Error: ' + response.status);
       }
     } catch (error) {
       // Manejar errores de la solicitud
@@ -28,18 +29,55 @@ const Login = () => {
   };
 
   return (
-    <Container>
-      <h3>Login</h3>
-      <FormGroup>
-        <label>Email:</label>
-        <input className="form-control" type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      </FormGroup>
-      <FormGroup>
-        <label>Password:</label>
-        <input className="form-control" type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </FormGroup>
-      <Button color="primary" onClick={handleLogin}>Iniciar sesión</Button>
-    </Container>
+    <div className="container-fluid bg-image">
+      <div class="container">
+      <div className="row justify-content-start">
+        <div className="col-lg-4 col-md-6 col-sm-8">
+          <div className="card" style={{ backgroundColor: "rgba(240, 240, 240, 0.9)", border: "1px solid #e2e5ec", borderRadius: "10px", padding: "20px", maxWidth: "400px", margin: "0 auto" }}>
+            <h2 className="card-title">Bienvenido</h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="email">Nombre de usuario:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  placeholder="Correo electrónico"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Contraseña:</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  placeholder="Contraseña"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mt-2">
+                <Button color="primary" className="btn-login" onClick={handleLogin}>
+                  Iniciar sesión
+                </Button>
+              </div>
+              <div className="mt-1">
+                <p>
+                  ¿No tienes una cuenta? <a href="/register">Regístrate aquí</a>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
   );
 };
 
